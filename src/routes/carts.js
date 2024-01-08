8541f78003240fd924af66a854ce727f1083585f
santurosa <santu_rosa@outlook.com>
2024-01-08 18:59:38 -0300

    update docs

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -4,14 +4,14 @@ import { applyPolicy } from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get("/tickets", applyPolicy(['ADMIN', 'USER', 'USER_PREMIUM']), getTicketsByEmail);
+router.get("/tickets", applyPolicy(['USER', 'USER_PREMIUM']), getTicketsByEmail);
 router.get("/:cid", applyPolicy(['PUBLIC']), getCart);
 router.post("/", applyPolicy(['PUBLIC']), createCart);
 router.post("/:cid/purchase", applyPolicy(['USER', 'USER_PREMIUM']), purchase);
 router.put("/:cid/product/:pid", applyPolicy(['USER', 'USER_PREMIUM']), upgrateCart);
 router.put("/:cid", applyPolicy(['USER', 'USER_PREMIUM']), upgrateCartByBody);
 router.put("/:cid/products/:pid", applyPolicy(['USER', 'USER_PREMIUM']), updateQuantityProducts);
-router.delete("/:cid", emptyCart);
-router.delete("/:cid/product/:pid", deleteProductToCart);
+router.delete("/:cid", applyPolicy(['PUBLIC']), emptyCart);
+router.delete("/:cid/product/:pid", applyPolicy(['PUBLIC']), deleteProductToCart);
 
 export default router;
