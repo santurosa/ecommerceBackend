b5eda25d9167982101d40f17378dd652456b3bde
santurosa <santu_rosa@outlook.com>
2024-01-08 23:53:54 -0300

    update restartPassword

--- src/public/js/restartPassword.js
+++ src/public/js/restartPassword.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', e => {
     const obj = {};
     data.forEach((value, key) => obj[key] = value);
     
-    fetch('/api/sessions/restartPassword', {
+    fetch('/api/users/restartPassword', {
         method: 'POST',
         body: JSON.stringify(obj),
         headers: {
